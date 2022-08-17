4947856233b52b558921105797fb301393409047
AurorePaladin <aurorepaladin@gmail.com>
2022-08-17 23:13:57 +0800

    vue3/选择详情

--- vue3/src/pages/select/index.vue
+++ vue3/src/pages/select/index.vue
@@ -3,26 +3,29 @@
     <div class="select-content">
       <LeftNav />
 
+      <selectDetail />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import LeftNav from './components/leftNav.vue'
+import LeftNav from "./components/leftNav.vue";
+import selectDetail from "./components/selectDetail.vue";
 export default {
-  name: 'index',
+  name: "index",
   components: {
     LeftNav,
+    selectDetail,
   },
 };
 </script>
 
 <style lang="scss">
-.select{
-  &-container{
+.select {
+  &-container {
     margin-top: 50px;
   }
-  &-content{
+  &-content {
     width: 1200px;
     margin: 0 auto;
     display: flex;
