1af356931d828c6a05f9982021ea1a75035c1def
AurorePaladin <aurorepaladin@gmail.com>
2022-08-19 23:20:10 +0800

    React-15-资讯tab

---
+++ react15/src/pages/information/Information.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { main } from '../../utils/global'
+import './Information.scss'
+
+// 添加资讯页面导航内容
+import InformationNav from './components/tab/index.jsx'
+
+class Information extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+  componentDidMount() {
+    // 设置面包屑内容
+    main.appInfo.crumbsState.setCrumbs(['首页', '选车', '丰田', '凯美瑞', '资讯列表'])
+  }
+
+  render() {
+    return (
+      <div className="information-container">
+        <div className="information-content">
+          <div className="information-wrapper">
+            {/* 资讯tab */}
+            <InformationNav />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Information
