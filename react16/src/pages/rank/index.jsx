8ec9fb0f87539a17df0abb192f9369f5007cd6f8
AurorePaladin <aurorepaladin@gmail.com>
2022-09-06 23:16:24 +0800

    排行

---
+++ react16/src/pages/rank/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.scss"
+
+const Rank = () => {
+  return (
+    <div className="rank-container">
+      <div className="rank-content">
+        123
+      </div>
+    </div>
+  );
+};
+
+export default Rank;
