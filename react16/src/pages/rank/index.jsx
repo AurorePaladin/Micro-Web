af2cb278f9b58677e9d689c86a761c49c33d31e3
AurorePaladin <aurorepaladin@gmail.com>
2022-09-10 16:20:15 +0800

    汽车排行

--- react16/src/pages/rank/index.jsx
+++ react16/src/pages/rank/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import CarType from "./components/CarType.jsx";
+import RankList from "./components/RankList.jsx";
 import "./index.scss"
 
 const Rank = () => {
   return (
     <div className="rank-container">
       <div className="rank-content">
-        123
+        <CarType />
+        <RankList />
       </div>
     </div>
   );
